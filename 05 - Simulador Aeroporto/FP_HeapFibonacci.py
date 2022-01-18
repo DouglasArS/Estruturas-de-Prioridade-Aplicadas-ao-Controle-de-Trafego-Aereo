f8c2e32e819e9c6e8916540d09d2f1bde01c75cb
Douglas Araújo <douglas.as.016c@hotmail.com>
2022-01-18 17:31:40 -0300

    Update FP_HeapFibonacci.py

--- 05 - Simulador Aeroporto/FP_HeapFibonacci.py
+++ 05 - Simulador Aeroporto/FP_HeapFibonacci.py
@@ -190,35 +190,3 @@ class FP_HeapFibonacci:
                 if not(no_atual != self.mini and no_atual.direita != None):
                     break  
             True
-
-if __name__ == "__main__":
-    
-    total_de_interacao = 10
-    tempo_acumulado = 0
-
-    for i in range(0, total_de_interacao):
-        tempo_inicial = time.time()
-
-        fp = FP_HeapFibonacci()
-
-        for j in range(1, 10000):
-            fp.inserir(nome = "A" + str(j), prioridade = j)            
-
-        for j in range(0, 5000):
-            fp.remover()
-            fp.consultar()
-
-        for j in range(1, 10000):
-            fp.inserir(nome = "B" + str(j), prioridade = j)
-            fp.consultar()
-
-        for j in range(0, 5000):
-            fp.remover()
-            fp.consultar()
-
-        for j in range(0, 10000):
-            fp.remover()
-
-        tempo_acumulado += (time.time() - tempo_inicial)
-
-    print("Tempo = {:.2f}", tempo_acumulado / total_de_interacao)
